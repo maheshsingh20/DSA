25f1a827849c00749e5430855307bf7847228c42
maheshsingh20 <singhmahesh2924@gmail.com>
2025-01-29 17:54:17 +0530

    second change

--- Strings.cpp
+++ Strings.cpp
@@ -1,102 +1,50 @@
-// #include <iostream>
-// #include <cstring>
-// using namespace std;
-
-
-// int main(){
-//     //character storing 
-//     // char a='a';
-//     // char b='b';
-//     // cout<<int(a)<<endl;
-//     // cout<<a<<endl<<b<<endl;
-//     // char f='f';
-//     // int pos=f-'a'+1;
-//     // cout<<pos;
-
-
-//     //char Arrays
-
-//     // char str[5]={'c','o','d','e','\0'};
-//     // char str1[]="hello";
-//     // cout<<strlen(str1)<<endl;
-//     // cout<<str;
-    
-
-//     char name[20];
-//     cout<<"Enter name: ";
-//     cin.getline(name,20,'g'); //name: arr name  20: size of array  g: limiter
-//     // cout<<name<<endl;
-//     // cout<<strlen(name);
-
-
-//     //convert to uppercase
-    
-    
-// }
-
-
-
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
-string mergealternatively(string a,string b){
-    string result="";
-    for(int i=0;i<a.length() && i<b.length();i++){
-        result+=a[i];
-        result+=b[i];
-    }
-    return result;
-}
 
-long long colorball(string s){
-    int cnt=0;
-    long long res=0;
-    for(const char c:s){
-        if(c=='1'){
-            ++cnt;
-        }else{
-            res+=cnt;
-        }
-    }
-    return res;
-}
+
+
 
 int main(){
+    /*
+    string str1;
+    getline(cin,str1);//getting the first string from the user
+    cout<<str1<<endl;
 
 
-//     string name="My name is Mahesh Singh";
-//     cout<<name<<endl;
-
-//     //length of string
-//     cout<<name.length()<<endl;
-
-//     //accessing a particular character
-//     cout<<name[0]<<endl;
-
-//     //changing a particular character
-//     name[0]='m';
-//     cout<<name<<endl;
-
-//     //concatenation
-//     string name1="Mahesh";
-//     string name2="Singh";
-//     string name3=name1+name2;
-//     cout<<name3<<endl;
-
-//     //compare
-//     string name4="Mahesh";
-//     string name5="Mahesh";
-//     if(name4+name5==name5+name4){
-//         cout<<"Strings are equal"<<endl;   
-//     }
-//     else{
-//         cout<<"Strings are not equal"<<endl;
-//     }
+    double num=stod(str1);// converting string to double
+    cout<<num<<endl;
 
 
-    string s="10111001";
-    cout<<colorball(s);
+    int num2=stoi(str1);// converting string to integer
+    cout<<num2<<endl;
+    
+    const double pi=3.14;
+    string str2="Value of pi is "+ to_string(pi);// converting double to string
+    cout<<str2<<endl;
 
+
+    
+
+
+    //APPEND 
+
+    string str1="Hello ";
+    string str2="World";
+    string str3="This is a string";
+    cout<<str1.append(str2)<<endl;  //append second word after first word
+    cout<<str1.append(5u,'.')<<endl;    // add 5 dots to the end of the string
+    cout<<str1.append(str3.begin()+6,str3.end())<<endl; //add the last 5 characters of str3 to str1
+
+
+    */
+
+   //OPERATOR +=
+
+    string str1="Hello ";
+    string str2="World";
+    str1+=str2;
+    cout<<str1<<endl;  //add str2 to str1
     return 0;
 }

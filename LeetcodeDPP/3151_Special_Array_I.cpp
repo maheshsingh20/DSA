7e974d5c7b45287ea8f5a5803a883da2b0fa9ca6
maheshsingh20 <singhmahesh2924@gmail.com>
2025-02-01 14:25:05 +0530

    01-feb-2025

---
+++ LeetcodeDPP/3151_Special_Array_I.cpp
@@ -0,0 +1,19 @@
+/*Easy level question*/
+
+cout << "isArraySpecial function called with input: ";
+for (int num : nums) {
+    cout << num << " ";
+}
+cout << endl;
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if(nums.size()==1)return true;
+        for(int i=0;i<nums.size()-1;i++){
+            if((nums[i]%2==0 && nums[i+1]%2==0)||(nums[i]%2!=0 && nums[i+1]%2!=0)){
+                return false;
+            }
+        }
+        return true;
+    }
+};

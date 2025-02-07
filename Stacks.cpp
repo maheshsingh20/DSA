26ba32a68e31bc500a86529c0f896da448b65a5c
maheshsingh20 <singhmahesh2924@gmail.com>
2025-02-07 15:12:00 +0530

    New Update

--- Stacks.cpp
+++ Stacks.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include <iostream>
 using namespace std;
 
@@ -117,4 +119,231 @@ int main() {
     s.print();
 
     return 0;
+}
+
+
+*/
+
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+/*Infix to Suffix*/
+
+int precedence(char ch)
+{
+    if (ch == '^')
+        return 3;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '+' || ch == '-')
+        return 1;
+    return -1;
+}
+
+string INFIX_POSTFIX(string s)
+{
+    stack<char> st; // stack for operators
+    string ans = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            ans += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push('(');
+        }
+        else if (s[i] == ')')
+        {
+            while (st.top() != '(')
+            {
+                ans += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+        {
+            while (!st.empty() && precedence(s[i]) <= precedence(st.top()))
+            {
+                ans += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+    return ans;
+}
+
+/*Infix to Prefix */
+
+string INFIX_PREFIX(string s)
+{
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ')')
+            s[i] = '(';
+        else if (s[i] == '(')
+            s[i] = ')';
+    }
+    stack<char> st;
+
+    string ans = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            ans += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push('(');
+        }
+        else if (s[i] == ')')
+        {
+            while (st.top() != '(')
+            {
+                ans += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+        {
+            while (!st.empty() && precedence(s[i]) < precedence(st.top()))
+            {
+                ans += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+/*Prefix to infix*/
+
+string PREFIX_INFIX(string s)
+{
+    string ans = "";
+    stack<string> st;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            st.push(string(1, s[i]));
+        }
+        else
+        {
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string temp = "(" + op1 + s[i] + op2 + ")";
+            st.push(temp);
+        }
+    }
+    ans += st.top();
+    return ans;
+}
+
+/*Postfix to Infix*/
+string POSTFIX_INFIX(string s)
+{
+    string ans = "";
+    stack<string> st;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            st.push(string(1, s[i]));
+        }
+        else
+        {
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string temp = "(" + op2 + s[i] + op1 + ")";
+            st.push(temp);
+        }
+    }
+    ans += st.top();
+    return ans;
+}
+
+/*Prefix to postFix*/
+string PREFIX_POSTFIX(string s)
+{
+    stack<string> st;
+    string ans = "";
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            st.push(string(1, s[i]));
+        }
+        else
+        {
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string temp = op1 + op2 + s[i];
+            st.push(temp);
+        }
+    }
+    ans += st.top();
+    return ans;
+}
+
+/*post fix to prefix */
+string POSTFIX_PREFIX(string s)
+{
+    stack<string> st;
+    string ans = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '1' && s[i] <= '9'))
+        {
+            st.push(string(1, s[i]));
+        }
+
+        else
+        {
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string temp = s[i] + op2 + op1;
+            st.push(temp);
+        }
+    }
+    ans += st.top();
+    return ans;
+}
+
+int main()
+{
+    cout << INFIX_POSTFIX("(3-2+l/8)-(4+6/3*(5^8))") << endl;
+    cout << INFIX_PREFIX("(3-2+l/8)-(4+6/3*(5^8))") << endl;
+    cout << PREFIX_INFIX("-+-32/l8+4*/63^58") << endl;
+    cout << POSTFIX_INFIX("32-l8/+463/58^*+-") << endl;
+    cout << PREFIX_POSTFIX("-+-32/l8+4*/63^58") << endl;
+    cout << POSTFIX_PREFIX("32-l8/+463/58^*+-") << endl;
 }

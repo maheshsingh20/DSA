c30d1301550d5c707dba794f32094d1a560ce0a9
Mahesh Singh <singhmahesh2924@gmail.com>
2025-07-28 23:30:31 +0530

    28-July-2025

--- Graphs/BellmanFord.cpp
+++ Graphs/BellmanFord.cpp
@@ -1,22 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct edge {
+    int u, v, w;
+};
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int n, m;
-  cin >> n >> m;
-  vector<vector<int>> edges(m, vector<int>(3));
-  for (int i = 0; i < m;i++){
-    cin >> edges[i][0] >> edges[i][1] >> edges[i][2];
-  }
-  vector<int> dist(n, INT_MAX);
-  for (int i = 0; i < n - 1;i++){
-    for(auot edge:edges){
-      int u = edge[0], v = edge[1], w = edge[2];
-      if(dis)
+int main() {
+    int node = 8;
+    int conn = 10;
+
+    vector<edge> Edge = {
+        {0, 1, 3}, {0, 4, 1}, {0, 2, 3}, {1, 2, 1}, {4, 5, 1},
+        {2, 3, 1}, {5, 3, 1}, {5, 6, 2}, {3, 7, 3}, {6, 7, 1}
+    };
+
+    vector<int> dist(node, INT_MAX);
+    dist[0] = 0; // source
+
+    for (int i = 0; i < node - 1; i++) {
+        for (auto &[u, v, w] : Edge) {
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
     }
-  }
+
+    for (int i : dist) {
+        if (i == INT_MAX)
+            cout << "INF ";
+        else
+            cout << i << " ";
+    }
+
+    return 0;
 }
--- Graphs/TopologicalSort.cpp
+++ Graphs/TopologicalSort.cpp
@@ -1,57 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
   int node = 5;
   int edge = 9;
   unordered_map<int, vector<int>> mp;
-  mp[0] = {1,2,3};
-  mp[1] = {3,4};
-  mp[2] = {3,4};
+  mp[0] = {1, 2, 3};
+  mp[1] = {3, 4};
+  mp[2] = {3, 4};
   mp[3] = {0};
   mp[4] = {3};
-  queue <int> qt;
+
+  queue<int> qt;
   vector<int> indeg(node, 0);
-  for (int i = 0; i < node;i++)
-  {
-    vector<int> nums = mp[i];
-    for(int j:nums){
+
+  for (int i = 0; i < node; i++) {
+    for (int j : mp[i]) {
       indeg[j]++;
     }
   }
+
   cout << "Indegree Array is: ";
-  for (int i = 0; i < node;i++){
+  for (int i = 0; i < node; i++) {
     cout << indeg[i] << " ";
   }
   cout << endl;
-  qt.push(0);
+
+  // Push all nodes with indegree 0
+  for (int i = 0; i < node; i++) {
+    if (indeg[i] == 0) {
+      qt.push(i);
+    }
+  }
+
   vector<int> res;
-  while(!qt.empty()){
+  while (!qt.empty()) {
     int curr = qt.front();
     qt.pop();
     res.push_back(curr);
-    for(int i:mp[curr]){
+    for (int i : mp[curr]) {
       indeg[i]--;
-      if(indeg[i]==0){
+      if (indeg[i] == 0) {
         qt.push(i);
       }
     }
   }
-  cout << "Tropological order of graph is : ";
-  for(int i:res){
+
+  cout << "Topological order of graph is: ";
+  for (int i : res) {
     cout << i << " ";
   }
   cout << endl;
+
   bool isCycle = false;
-  for (int i = 0; i < node;i++){
-    if(indeg[i]!=0){
-      isCycle=true;
+  for (int i = 0; i < node; i++) {
+    if (indeg[i] != 0) {
+      isCycle = true;
       break;
     }
   }
-  if( isCycle){
+
+  if (isCycle) {
     cout << "Cycle Detected" << endl;
-  }else{
+  } else {
     cout << "Cycle not found" << endl;
   }
+
   return 0;
 }
